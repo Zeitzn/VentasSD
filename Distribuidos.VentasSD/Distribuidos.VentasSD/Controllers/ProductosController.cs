1d21962f37ce94b9dd0b113c1466ec699a0b2084
Isac <smevy4lc3@gmail.com>
2018-06-08 18:11:00 -0500

    pp

--- Distribuidos.VentasSD/Distribuidos.VentasSD/Controllers/ProductosController.cs
+++ Distribuidos.VentasSD/Distribuidos.VentasSD/Controllers/ProductosController.cs
@@ -6,34 +6,59 @@ using System.Web.Mvc;
 
 namespace Distribuidos.VentasSD.Controllers
 {
-    public class PersonasController : Controller
+    public class ProductosController : Controller
     {
         private readonly JavaService.WebServiceDCPClient java = new JavaService.WebServiceDCPClient();
 
         public ActionResult Index()
         {
-            return View(java.PersonaMostrarAll());
+            
+            return View(java.ProductoMostrarAll());
         }
 
-        // GET: Personas/Details/5
+        // GET: Productos/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: Personas/Create
+        // GET: Productos/Create
         public ActionResult Create()
+        {
+            ViewBag.Categorias = new SelectList(java.CategoriaMostrarAll(), "id", "nombre");
+            return View();
+        }
+
+        // POST: Productos/Create
+        [HttpPost]
+        public ActionResult Create(JavaService.producto model)
+        {
+            try
+            {
+
+                java.ProductoRegistrar(model.fk_idcategoria, model.codigo, model.nombre, model.descripcion, model.stock, model.condicion);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Productos/Edit/5
+        public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: Personas/Create
+        // POST: Productos/Edit/5
         [HttpPost]
-        public ActionResult Create(JavaService.persona model)
+        public ActionResult Edit(int id, FormCollection collection)
         {
             try
             {
-                java.PersonaRegistrar(model.tipo_persona, model.nombre, model.papellido, model.sapellido, model.tipo_doc, model.num_doc, model.direccion, model.telefono, model.email);
+                // TODO: Add update logic here
 
                 return RedirectToAction("Index");
             }
@@ -43,43 +68,19 @@ namespace Distribuidos.VentasSD.Controllers
             }
         }
 
-        // GET: Personas/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View(java.PersonaBuscarById(id));
-        }
-
-        // POST: Personas/Edit/5
-        [HttpPost]
-        public ActionResult Edit(JavaService.persona model)
-        {
-            try
-            {
-
-                java.PersonaActualizar(model.idpersona, model.tipo_persona, model.nombre, model.papellido, model.sapellido, model.tipo_doc, model.num_doc, model.direccion, model.telefono, model.email);
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Personas/Delete/5
+        // GET: Productos/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: Personas/Delete/5
+        // POST: Productos/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
             try
             {
-                
-               
+                // TODO: Add delete logic here
 
                 return RedirectToAction("Index");
             }
